93eacb305b67d5d250c2e8b250772db1f4504ad4
Marin Jelev <marin.jelev92@gmail.com>
2015-02-24 23:04:55 +0200

    Added alternative solutions

--- C# Part 2/MethodsHW/06. FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
+++ C# Part 2/MethodsHW/06. FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
@@ -77,3 +77,32 @@ class FirstLargerThanNeighbours
     }
 }
 
+//Alternative solution, 'ignoring' first and last position 
+//static bool CheckIfBigger(int[] array, int index)
+//    {
+//        if (index == 0 || index == array.Length - 1)
+//        {
+//            return false;
+//        }
+//        return ((array[index - 1] < array[index]) && (array[index + 1] < array[index]));
+//    }
+//    static int CheckArray(int[] array)
+//    {
+//        int index = -1;
+//        for (int i = 1; i < array.Length - 1; i++)
+//        {
+//            if (CheckIfBigger(array, i) == true)
+//            {
+//                index = i;
+//                break;
+//            }
+//        }
+//        return index;
+//    }
+
+//    static void Main()
+//    {
+//        int[] array = { 3, 3, 2, 19, 8, 4, 6 };
+//        int ind = CheckArray(array);
+//        Console.WriteLine(ind);
+//    }
--- C# Part 2/MethodsHW/09. SortingArray/SortingArray.cs
+++ C# Part 2/MethodsHW/09. SortingArray/SortingArray.cs
@@ -92,3 +92,45 @@ class SortingArray
 
 //static public int[] test = { 10, 2, 5, 10, 100, 40, 30, 90 };
 //                             0   1  2  3   4    5   6   7  length = 8
+
+
+//A way to sort array
+//static int FindMaxElementInInterval(int[] numbers, int start, int end, int swapIndex = 0)
+//        {
+//            if (start < 0 || start >= numbers.Length || end < 0 || end >= numbers.Length)
+//                throw new IndexOutOfRangeException();
+
+//            int maxIndex = start;
+
+//            for (int i = start; i <= end; i++)
+//                if (numbers[maxIndex] < numbers[i])
+//                    maxIndex = i;
+
+//            int temp = numbers[swapIndex];
+//            numbers[swapIndex] = numbers[maxIndex];
+//            numbers[maxIndex] = temp;
+
+//            return numbers[swapIndex];
+//        }
+
+//        static int[] SortAscending(int[] numbers)
+//        {
+//            int size = numbers.Length;
+//            int[] sorted = new int[size];
+
+//            for (int i = size - 1; i >= 0; i--)
+//                sorted[i] = FindMaxElementInInterval(numbers, 0, i, i);
+
+//            return sorted;
+//        }
+
+//        static int[] SortDescending(int[] numbers)
+//        {
+//            int size = numbers.Length;
+//            int[] sorted = new int[size];
+
+//            for (int i = 0; i < size; i++)
+//                sorted[i] = FindMaxElementInInterval(numbers, i, size - 1, i);
+
+//            return sorted;
+//        }
