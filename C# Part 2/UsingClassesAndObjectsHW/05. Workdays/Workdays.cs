81c99847653c7956c583e1961cd85c593f196fd5
Marin Jelev <marin.jelev92@gmail.com>
2015-02-25 14:14:04 +0200

    Added problems 5 and 6

--- C# Part 2/UsingClassesAndObjectsHW/05. Workdays/Workdays.cs
+++ C# Part 2/UsingClassesAndObjectsHW/05. Workdays/Workdays.cs
@@ -1,6 +1,5 @@
 ﻿namespace Workdays
 {
-
     using System;
     using System.Collections.Generic;
     using System.Globalization;
@@ -19,21 +18,35 @@
         {
             Console.Write("Choose a future date (dd/MM/yyyy): ");
             string inputFutureDate = Console.ReadLine(); ;
-            string format = "dd/MM/yyyy";
+            string format = "dd/MM/yyyy"; //format of date
             DateTime futureDate = DateTime.ParseExact(inputFutureDate, format, CultureInfo.InvariantCulture);
-            GetWorkdays(futureDate);
 
-           
+            DateTime currentDate = DateTime.Now.Date;
 
-        }
+            var dates = new List<DateTime>(); //empty list to add DATES inbetween current and future date
+            var daysOfWeek = new List<DayOfWeek>(); //empty list to add DAYS inbetween current and future date
 
-        static void GetWorkdays(DateTime futureDate)
-        {
-            DateTime currentDate = DateTime.Now;
+            //List of some Bulgarian holidays in 2015 (Baba Marta, Liberation Day, St. George Day):
+            var holidays = new List<DateTime>(){new DateTime(2015, 03, 03), new DateTime(2015, 03, 01), new DateTime(2015, 05, 06)};
 
-            System.TimeSpan difference = futureDate.Subtract(currentDate);
+            for (var date = currentDate; date <= futureDate; date = date.AddDays(1)) //'<=' since futureDate is included
+            {
+                dates.Add(date);
+                daysOfWeek.Add(date.DayOfWeek);
+                for (int i = 0; i < holidays.Count; i++)
+                {
+                    if (dates.Contains(holidays[i]))
+                    {
+                        dates.Remove(date);
+                        daysOfWeek.Remove(date.DayOfWeek);
+                    }
+                    
+                }
+            }
+            daysOfWeek.Remove(DayOfWeek.Saturday); //Removing Saturday
+            daysOfWeek.Remove(DayOfWeek.Sunday); //Removing Sunday
 
-            Console.WriteLine(difference.);
+            Console.WriteLine("\nTotal number of working days: {0}", daysOfWeek.Count);
         }
     }
 }
--- C# Part 2/UsingClassesAndObjectsHW/06. SumIntegers/SumIntegers.cs
+++ C# Part 2/UsingClassesAndObjectsHW/06. SumIntegers/SumIntegers.cs
@@ -1,22 +1,37 @@
 ﻿namespace SumIntegers
 {
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-//Problem 6. Sum integers
+    //Problem 6. Sum integers
 
-//You are given a sequence of positive integer values written into a string, separated by spaces.
-//Write a function that reads these values from given string and calculates their sum.
+    //You are given a sequence of positive integer values written into a string, separated by spaces.
+    //Write a function that reads these values from given string and calculates their sum.
 
     class SumIntegers
     {
         static void Main()
         {
+            //User input
+            //Console.Write("Please input integers, separated by space: ");
+            //string input = Console.ReadLine();
 
+            //Example from problem
+            string input = "43 68 9 23 318";
+
+            string[] result = input.Split(' ');
+            int sum = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                sum += int.Parse(result[i]);
+            }
+
+            Console.WriteLine("The sum of integers is: {0}", sum);
         }
     }
 }
