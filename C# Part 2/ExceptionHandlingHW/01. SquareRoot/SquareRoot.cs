c8bab2104fd043effc105b16a640e53ffacc9437
Marin Jelev <marin.jelev92@gmail.com>
2015-03-10 12:38:27 +0200

    Added ExceptionHandlingHW

---
+++ C# Part 2/ExceptionHandlingHW/01. SquareRoot/SquareRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace SquareRoot
+{
+//Problem 1. Square root
+
+//Write a program that reads an integer number and calculates and prints its square root.
+//If the number is invalid or negative, print Invalid number.
+//In all cases finally print Good bye.
+//Use try-catch-finally block.
+
+    using System;
+    using System.Text;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
