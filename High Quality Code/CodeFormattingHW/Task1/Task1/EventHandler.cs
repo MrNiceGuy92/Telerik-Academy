52e8dfd02243210f21f9b21adee2acb88b22078a
Marin Jelev <marin.jelev92@gmail.com>
2015-06-21 10:01:15 +0300

    Added HQC Folder + Task1

---
+++ High Quality Code/CodeFormattingHW/Task1/Task1/EventHandler.cs
@@ -0,0 +1,56 @@
+﻿namespace Task1
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Event> dictionaryByTitle = new MultiDictionary<string, Event>(true);
+        private OrderedBag<Event> dictionaryByDate = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+            this.dictionaryByTitle.Add(title.ToLower(), newEvent);
+            this.dictionaryByDate.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in this.dictionaryByTitle[title])
+            {
+                removed++;
+                this.dictionaryByDate.Remove(eventToRemove);
+            }
+
+            this.dictionaryByTitle.Remove(title);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.dictionaryByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
